7c5be2de097da47369bc6432cf1693de0c381982
Bryan Moreno <bfourbryan@gmail.com>
2021-02-20 02:54:08 -0800

    Addition of id tags in the current weather card, adding momentjs script tag, html. Using moment to get current time, beginning append of api info to current weather card, still working on iteritive key for saved cities.js

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
 console.log("taylor swift");
 const apiKEY = "ad269f335fe283efea967260b3287c51";
+const findCityBtn = $(".findBtn");
+const time = moment();
+const currentDate = time.format("MMM Do");
+console.log(currentDate);
+
+//When page loads the last searched city should append to the appropriate elements
+
+//When the page loads the list of previously searched cities appears in a list
 
 // When the user clicks the search button
-const findCityBtn = $(".findBtn");
-
-console.log(findCityBtn);
-
 findCityBtn.click(function() {
     var cityInput = $(this).prev("input").val();
-    console.log(cityInput);
+    // var i = 0;
+    // for (i = 0; i < localStorage.length; i++) {
+    //     console.log(cityInput);
+    //     var cityKeyNum = "city-" + i;
+    // };
     localStorage.setItem("city", cityInput);
-    handleSearch();
-});
-// const cityNameSearched = $(".findBtn").prev("input").val();
-// console.log(cityNameSearched);
-
-// findCityBtn.on("click", function (event) {
-//     var clickedButton = $(this);
-//     // var hourButtonClicked = $(clickedButton).siblings("div").data("time");
-//     const eventInput = $(clickedButton).prev("input").val();
-//     // localStorage.setItem("scheduled-hour-" + hourButtonClicked, eventInput);
-//     console.log(eventInput);
-//     console.log(clickedButton);
-//     handleSearch();
-// });
-function handleSearch() {
-    console.log("Britney Spears")
-    // Then GET the user input VALUE they entered
-    localStorage.getItem("city");
-    console.log(localStorage.getItem("city"));
+    
+    // handleSearch();
     makeWeatherRequest();
-};
+});
+
+// function handleSearch() {
+//     console.log("Britney Spears")
+//     // Then GET the user input VALUE they entered
+//     localStorage.getItem("city");
+//     console.log(localStorage.getItem("city"));
+//     makeWeatherRequest();
+// };
 
 
 function makeWeatherRequest( city ){
-
+    
+    // var i = 0;
+    // for (i = 0; i < localStorage.length; i++) {
+        
+    //     var cityKeyNum = "city-" + i;
+    // }
     var cityNameSearched = localStorage.getItem("city");
     console.log(cityNameSearched);
     console.log("Shawn Mendez");
     // NEXT, we need to build the url for the first api request
-    const cityNameAPIReq = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameSearched}&appid=${apiKEY}`;
+    const cityNameAPIReq = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameSearched}&units=imperial&appid=${apiKEY}`;
     
     //    replace the users input with the location they wish to search for
     
@@ -51,6 +55,29 @@ function makeWeatherRequest( city ){
     }).then(function(response) {
         
         // START rendering data to the html
+        var cityName = response.name;
+        var countryCode = response.sys.country;
+        var cityTemp = response.main.temp;
+        var cityHum = response.main.humidity;
+        var cityWindy = response.wind.speed;
+        // var cityUV = response
+        //clears out the element to be used
+        $("#currentCityName").empty();
+        $("#currentCityDate").empty();
+        $("#currentCityTemp").empty();
+        $("#currentCityHum").empty();
+        $("#currentCityWind").empty();
+        // $("#currentCityUV").empty();
+
+        //Append api data to element
+        $("#currentCityName").append(cityName, ", ", countryCode);
+        $("#currentCityDate").append(currentDate);
+        $("#currentCityTemp").append(cityTemp);
+        $("#currentCityHum").append(cityHum);
+        $("#currentCityWind").append(cityWindy);
+        // $("#currentCityUV").append();
+
+        
         
         // THEN get the lat and lng out of the response object
         var lat = response.coord.lat;
@@ -71,7 +98,7 @@ function makeOneCallRequest() {
     var lon = localStorage.getItem("lon");
     console.log(lat,lon);
     // NEXT, we need to build the url for the first api request
-    const latLonAPIReq = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKEY}`;
+    const latLonAPIReq = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKEY}`;
     //    replace the users input with the location they wish to search for
     
     // Next, make the request to the URL with jQuery ajax
